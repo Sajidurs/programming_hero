4b9f2d5f16ffc163ad0fd251b2f9d86aec0ba7ca
Sajidur Rahman <78880964+Sajidurs@users.noreply.github.com>
2023-06-13 00:02:55 +0600

    Lastnight

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+
+
+
+// JS for the client's logo looping
+$(function(){
+    $('#example').jellySlider();
+  });
+  $(function(){
+    $('#example').jellySlider({
+      loop: true
+    });
+  });
+  $(function(){
+      $('#example').jellySlider({
+        drag:true
+      });
+    });
+    $(function(){
+        $('#example').jellySlider({
+          margin: 10
+        });
+      });
+    
+    
